14b86e4bd3e7a8ad0343bd746c374b042c38cef2
Ji Chen <jaykchen@gmail.com>
2023-08-13 21:49:15 -0400

    clean-up for team review

--- src/discord_functions.rs
+++ src/discord_functions.rs
@@ -4,6 +4,9 @@ use serde_json;
 use std::env;
 
 pub async fn register_commands(discord_token: &str) -> bool {
+    let bot_id = env::var("bot_id").unwrap_or("1124137839601406013".to_string());
+    let guild_id = env::var("discord_server").unwrap_or("1128056245765558364".to_string());
+
     let command_weekly_report = serde_json::json!({
         "name": "weekly_report",
         "description": "Generate a weekly report",
@@ -30,53 +33,8 @@ pub async fn register_commands(discord_token: &str) -> bool {
         ]
     });
 
-    let command_get_user_repos = serde_json::json!({
-        "name": "get_user_repos",
-        "description": "Get user's top repos by programming lanugage",
-        "options": [
-            {
-                "name": "username",
-                "description": "The username to lookup",
-                "type": 3,
-                "required": true
-            },
-            {
-                "name": "language",
-                "description": "The repo's programming language",
-                "type": 3,
-                "required": true
-            }
-        ]
-    });
-
-    let command_search = serde_json::json!({
-        "name": "search",
-        "description": "Search for mentions in issues",
-        "options": [
-            {
-                "name": "search_query",
-                "description": "The query to search mentions for",
-                "type": 3, // String type according to Discord's API
-                "required": true
-            },
-            {
-                "name": "search_type",
-                "description": "The type to search mentions in (e.g., ISSUE)",
-                "type": 3, // String type according to Discord's API
-                "required": true
-            }
-        ]
-    });
-
-    let bot_id = env::var("bot_id").unwrap_or("1124137839601406013".to_string());
-    // let channel_id = env::var("discord_channel_id").unwrap_or("1128056246570860617".to_string());
-    let guild_id = env::var("discord_guild_id").unwrap_or("1128056245765558364".to_string());
     let guild_id = guild_id.parse::<u64>().unwrap_or(1128056245765558364);
-    let commands = serde_json::json!([
-        command_weekly_report,
-        command_get_user_repos,
-        command_search,
-    ]);
+    let commands = serde_json::json!([command_weekly_report,]);
     let http_client = HttpBuilder::new(discord_token)
         .application_id(bot_id.parse().unwrap())
         .build();
